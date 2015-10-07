e11d5987a85dd74f93e8bbc31b1d08697e269893
viictor22 <viictor_54@hotmail.com>
2015-10-07 19:04:44 +0200

    ejercicio11

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,7 @@
 public class hellow_world {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo from vilanova");
+        System.out.println("jelou uol from vilanova");
 
      
     }
