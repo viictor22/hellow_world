81b18837c9dceea8d8452179c208b8656028c17d
viictor22 <viictor_54@hotmail.com>
2015-10-07 15:13:54 +0200

    ejercicio9

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 public class hellow_world {
     public static void main(String[] args) {
-        System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
+        System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
     }
 
 }
